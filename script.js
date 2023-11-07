af330a289cc06b975c09005308a8dfb5b3acd8ac
Amanuel Belete <amanuelbelete95@gmail.com>
2023-11-07 22:04:09 +0300

    Add functionality for adding recipe

--- script.js
+++ script.js
@@ -2,20 +2,44 @@ const input = document.querySelector('.recipe-input');
 const addBtn = document.querySelector('.add-recipe');
 const removeBtn = document.querySelector('remove-recipe');
 
-const recipeList = document.querySelectorAll('.list-recipe');
+const recipeContainer = document.querySelector('.recipe-list');
 
-let myMenu = [];
+const populateUX = () => {
+  if (localStorage.getItem('recipe')) {
+    const datas = JSON.parse(localStorage.getItem('recipe'));
+    datas.forEach((data) => {
+      //createElement function;
+      const pEle = document.createElement('p');
+      const remove = document.createElement('button');
+      pEle.textContent = data;
+      remove.textContent = 'remove';
+      recipeContainer.appendChild(pEle);
+      recipeContainer.appendChild(remove);
+    });
+  }
+};
 
-//create a function that holds the value from the input field and adds it to my menu;
+populateUX();
 
 const addRecipe = () => {
-  let recipe = input.value;
-  if (recipe) {
-    myMenu.push(recipe);
-  }
+  //check for an array if present add; push a new value to the array; get it  a back , parse and push  the new value to it;
 
+  let Menu = localStorage.getItem('recipe')
+    ? JSON.parse(localStorage.getItem('recipe'))
+    : [];
+
+  const recipeInput = input.value;
+  Menu.push(recipeInput);
+  localStorage.setItem('recipe', JSON.stringify(Menu));
+  const pEle = document.createElement('p');
+  pEle.textContent = recipeInput;
+  recipeContainer.appendChild(pEle);
+  const remove = document.createElement('button');
+  remove.textContent = 'remove';
+  recipeContainer.appendChild(remove);
   input.value = '';
-  console.log(myMenu);
 };
 
 addBtn.addEventListener('click', addRecipe);
+
+const appendElement = () => {};
