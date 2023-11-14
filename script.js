ea52f77f2c9010a4c8067461538f25d3dc51fb4e
Amanuel Belete <amanuelbelete95@gmail.com>
2023-11-14 20:38:39 +0300

    Add functionality to add, remove and edit recipe

--- script.js
+++ script.js
@@ -1,45 +1,97 @@
 const input = document.querySelector('.recipe-input');
 const addBtn = document.querySelector('.add-recipe');
-const removeBtn = document.querySelector('remove-recipe');
-
 const recipeContainer = document.querySelector('.recipe-list');
 
+const reSetAll = document.querySelector('.reSetAll');
+
 const populateUX = () => {
+  let parsedData = [];
   if (localStorage.getItem('recipe')) {
-    const datas = JSON.parse(localStorage.getItem('recipe'));
-    datas.forEach((data) => {
-      //createElement function;
+    parsedData = JSON.parse(localStorage.getItem('recipe'));
+    parsedData.forEach((data) => {
+      const contains = document.createElement('div');
       const pEle = document.createElement('p');
+
       const remove = document.createElement('button');
-      pEle.textContent = data;
+      pEle.textContent = data.recipe;
       remove.textContent = 'remove';
-      recipeContainer.appendChild(pEle);
-      recipeContainer.appendChild(remove);
+      remove.classList.add('remove');
+      contains.appendChild(pEle);
+      contains.appendChild(remove);
+      recipeContainer.appendChild(contains);
     });
   }
+
+  return parsedData;
 };
 
 populateUX();
 
+// remove functionality
+let id = 0;
 const addRecipe = () => {
-  //check for an array if present add; push a new value to the array; get it  a back , parse and push  the new value to it;
+  console.log('hello');
+  id += 1;
 
   let Menu = localStorage.getItem('recipe')
     ? JSON.parse(localStorage.getItem('recipe'))
     : [];
-
   const recipeInput = input.value;
-  Menu.push(recipeInput);
+  const recipeProp = {};
+  recipeProp.recipe = recipeInput;
+  recipeProp.recipe_id = id;
+
+  Menu.push(recipeProp);
   localStorage.setItem('recipe', JSON.stringify(Menu));
+  const contains = document.createElement('div');
   const pEle = document.createElement('p');
   pEle.textContent = recipeInput;
-  recipeContainer.appendChild(pEle);
   const remove = document.createElement('button');
+  contains.appendChild(pEle);
   remove.textContent = 'remove';
-  recipeContainer.appendChild(remove);
+  remove.classList.add(`remove`);
+  contains.appendChild(remove);
+  recipeContainer.appendChild(contains);
   input.value = '';
 };
 
 addBtn.addEventListener('click', addRecipe);
 
-const appendElement = () => {};
+// remove function
+
+recipeContainer.addEventListener('click', (event) => {
+  const parsedData = populateUX();
+
+  for (let i = 0; i < parsedData.length; i++) {
+    if (event.target.tagName === 'BUTTON') {
+      console.log(event.target.tagName);
+      const button = event.target;
+      const sec = button.parentNode;
+      recipeContainer.removeChild(sec);
+    }
+  }
+});
+/*
+const removeRecipe = (e) => {
+  const removeButton = e.target;
+  // const elementIndex = removeButton.parentNode.parentNode.children.indexOf(removeButton);
+  removeButton.parentNode.parentNode.removeChild(removeButton.parentNode);
+
+  const parsedData = populateUX();
+
+  parsedData.forEach((recipe, index) => {
+    recipe.recipe_id = index;
+  });
+
+  localStorage.setItem('recipe', JSON.stringify(currentList));
+};
+
+//if the document has loaded add event listeners to the
+
+if (document.readyState === 'complete') {
+  const removeButtons = document.querySelectorAll('.remove');
+  removeButtons.forEach((removeButton) => {
+    removeButton.addEventListener('click', removeRecipe);
+  });
+}
+*/
